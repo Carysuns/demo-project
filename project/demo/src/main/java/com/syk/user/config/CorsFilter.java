badf0e952948d0032a1445bf3bbf02c092dccf3e
sunyk <250037349@qq.com>
2021-07-24 23:42:48 +0800

    跨域拦截共通类修正

--- project/demo/src/main/java/com/syk/user/config/CorsFilter.java
+++ project/demo/src/main/java/com/syk/user/config/CorsFilter.java
@@ -1,5 +1,9 @@
 package com.syk.user.config;
 
+import org.apache.shiro.web.util.WebUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -8,6 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
@@ -21,15 +26,21 @@ public class CorsFilter implements Filter {
       throws IOException, ServletException {
 
     //解决跨域访问报错
+    HttpServletRequest httpServletRequest = WebUtils.toHttp(request);
     HttpServletResponse response = (HttpServletResponse) resp;
-    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Access-Control-Allow-Origin", httpServletRequest.getHeader("Origin"));
     response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
     // 设置过期时间
     response.setHeader("Access-Control-Max-Age", "3600");
-    response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, client_id, uuid, Authorization");
+    response.setHeader("Access-Control-Allow-Headers", httpServletRequest.getHeader("Access-Control-Allow-Headers"));
     // 支持HTTP 1.1.
     response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate");
     response.setHeader("Pragma", "no-cache"); // 支持HTTP 1.0. response.setHeader("Expires", "0");
+
+    if (httpServletRequest.getMethod().equals(RequestMethod.OPTIONS.name())) {
+
+      response.setStatus(HttpStatus.OK.value());
+    }
     chain.doFilter(request, resp);
   }
 
